c8ebf41c5495eb21ee221d78ab3867856aef5ccd
Kanna <91947146+blackcat096@users.noreply.github.com>
2022-01-15 19:53:28 +0530

    Update callback.py

--- program/callback.py
+++ program/callback.py
@@ -17,7 +17,7 @@ from config import (
 async def cbstart(_, query: CallbackQuery):
     await query.edit_message_text(
         f"""✨ **Welcome [{query.message.chat.first_name}](tg://user?id={query.message.chat.id}) !**\n
-💭 **[{BOT_NAME}](https://t.me/{BOT_USERNAME}) allows you to play music and video on groups through the new Telegram's video chats!**
+💭 **[{BOT_NAME}](https://t.me/{BOT_USERNAME}) [🧐](https://telegra.ph/file/f4e1c2ebc7a14110faf47.jpg)**
 
 💡 **Find out all the Bot's commands and how they work by clicking on the » 📚 Commands button!**
 
